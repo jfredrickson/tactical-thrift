751d7447f3f3cd73330faf40bb8f00459715c001
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2016-09-19 15:54:39 -0400

    automatically compute the latest positions upon visit to the index page if positions do not yet exist

--- app.rb
+++ app.rb
@@ -4,10 +4,18 @@ class App < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
   get "/" do
-    @c_fund = Position.where(fund: "C").order(date: :desc).first
-    @s_fund = Position.where(fund: "S").order(date: :desc).first
-    @i_fund = Position.where(fund: "I").order(date: :desc).first
-    @funds = [@c_fund, @s_fund, @i_fund].compact
+    fund_names = ["C", "S", "I"]
+
+    # Make sure we have the latest positions. If not, update them.
+    first_day_of_month = Date.today.beginning_of_month
+    if Position.where(date: first_day_of_month).count != fund_names.count
+      Position.update_positions
+    end
+
+    # Get the positions and render them.
+    @funds = fund_names.collect { |fund_name|
+      Position.find_by(fund: fund_name, date: first_day_of_month)
+    }.compact
     erb :index
   end
 
