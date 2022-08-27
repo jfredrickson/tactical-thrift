9ca4e452fa86754dce176f3b0faefde201827530
jfredrickson <jfredrickson@users.noreply.github.com>
2022-08-27 21:15:51 +0000

    deploy: 5159aaa829f7ce46a982bed6bef8a68e2040627e

---
+++ assets/javascripts/charts.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  var charts = [];
+
+  function buildChartData(fundName, allPositions) {
+    const positions = allPositions
+      .filter(position => position.fund === fundName)
+      .slice(-12);
+
+    var data = {
+      labels: [],
+      datasets: []
+    };
+    var smaDataset = {
+      type: "line",
+      label: "10-month SMA",
+      data: [],
+      fill: false,
+      borderColor: "rgb(33, 133, 181)"
+    };
+    var priceDataset = {
+      type: "line",
+      label: "Price",
+      data: [],
+      fill: false,
+      borderColor: "rgb(35, 139, 69)"
+    };
+    positions.forEach(function (position) {
+      var date = formatDate(position.date);
+      var status = position.invested ? "Invested" : "Move to G";
+      data.labels.push([date, status]);
+      smaDataset.data.push(position.tenMonthAverage);
+      priceDataset.data.push(position.tenthMonthPrice);
+    });
+    data.datasets.push(smaDataset);
+    data.datasets.push(priceDataset);
+    return data;
+  }
+
+  function renderChart(targetElement, data) {
+    var chart = new Chart(targetElement, {
+      type: "line",
+      data: data
+    });
+  }
+
+  function formatDate(isoDate) {
+    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var dateComponents = isoDate.split("-");
+    var year = dateComponents[0];
+    var month = parseInt(dateComponents[1]);
+    return monthNames[month - 1] + " " + year;
+  }
+
+  $("canvas.chart").each(function () {
+    var fundName = $(this).data("fund");
+    var targetElement = $(this);
+    $.getJSON("/data/positions.json", function (positions) {
+      var data = buildChartData(fundName, positions);
+      renderChart(targetElement, data);
+    });
+  });
+});
